c62e835ce68843d4de886761dc85102a419c60ff
b3hr4d <behradmusiclove@yahoo.com>
2025-08-10 00:22:49 +0300

    Refactor code structure for improved readability and maintainability

--- examples/C#/example.cs
+++ examples/C#/example.cs
@@ -2,35 +2,30 @@ using System;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace ExampleNamespace
+private void OnCommitListDoubleTapped(object sender, TappedEventArgs e)
 {
-    public class ExampleClass
+    // This is a comment
+    e.Handled = true;
+    int a = 0;
+    a = a + 1;
+
+    if (!(DataContext is ViewModels.Histories histories &&
+            CommitListContainer.SelectedItems is { Count: 1 } &&
+            sender is DataGrid grid &&
+            !Equals(e.Source, grid)))
     {
-        private void OnCommitListDoubleTapped(object sender, TappedEventArgs e)
-        {
-            e.Handled = true;
-            int a = 0;
-            a = a + 1;
+        return;
+    }
 
-            if (!(DataContext is ViewModels.Histories histories &&
-                  CommitListContainer.SelectedItems is { Count: 1 } &&
-                  sender is DataGrid grid &&
-                  !Equals(e.Source, grid)))
-            {
-                return;
-            }
+    if (e.Source is CommitRefsPresenter crp)
+    {
+        var decorator = crp.DecoratorAt(e.GetPosition(crp));
+        if (histories.CheckoutBranchByDecorator(decorator))
+            return;
+    }
 
-            if (e.Source is CommitRefsPresenter crp)
-            {
-                var decorator = crp.DecoratorAt(e.GetPosition(crp));
-                if (histories.CheckoutBranchByDecorator(decorator))
-                    return;
-            }
-
-            if (e.Source is Control { DataContext: Models.Commit c })
-            {
-                histories.CheckoutBranchByCommit(c);
-            }
-        }
+    if (e.Source is Control { DataContext: Models.Commit c })
+    {
+        histories.CheckoutBranchByCommit(c);
     }
 }
